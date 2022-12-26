a682600e76a75c8f960b1dab2ae5d00b86506a2c
Tran Nhat Anh <anhtran17102003@gmail.com>
2022-12-26 21:30:08 +0700

    Build main discovery page #1

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,18 +1,19 @@
 import Header from '../components/Header';
 import Sidebar from './Sidebar';
 import NowPlayingBar from '../components/NowPlayingBar';
-import MainPage from '../components/MainPage';
+import styles from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind';
+import Discovery from '~/pages/Discovery';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
         <div>
             <Header />
-            <div className="container" style={{ width: '100%', height: '100vh' }}>
-                <Sidebar />
-                <MainPage />
-                <div className="content">{children}</div>
-            </div>
-            <NowPlayingBar />
+            <Sidebar />
+            <Discovery />
+            {/* <NowPlayingBar /> */}
         </div>
     );
 }
