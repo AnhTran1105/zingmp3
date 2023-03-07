14db9cdc3a97a8d810e4c73567df62d350715f6f
Tran Nhat Anh <anhtran17102003@gmail.com>
2023-03-07 21:48:48 +0700

    Build animation of aritists section #2

--- src/components/Layout/components/GallerySection/index.js
+++ src/components/Layout/components/GallerySection/index.js
@@ -4,41 +4,76 @@ import { useState, useEffect, useRef } from 'react';
 
 const cx = classNames.bind(styles);
 
-let offSetValues = [0, 0, 0, 0, 0, 0];
+const offSetValues = [0, 0, 0, 0, 0, 0];
+let count = 0;
+let clicked = false;
 
 function GallerySection({ props }) {
-    const [offSetX, setOffSetX] = useState(offSetValues[0]);
+    const [offSetX, setOffSetX] = useState(offSetValues);
     const [time, setTime] = useState(Date.now());
+    const galleryItems = useRef([]);
+    const prevOffSetX = useRef([]);
 
-    const galleryItem = useRef([]);
+    // 200 100 0
 
     function changeArray(arr) {
+        let flag = false;
         for (var i = 0; i < arr.length; i++) {
-            if (arr[i] <= -300) {
+            if (arr[i] <= -300 && flag === false) {
+                flag = true;
                 arr[i] = 300 - i * 100;
-                break;
             }
-        }
-
-        for (var j = 0; j < arr.length; j++) {
-            arr[j] = arr[j] - 100;
+            arr[i] = arr[i] - 100;
         }
         return arr;
     }
 
     useEffect(() => {
-        const interval = setInterval(() => setTime(Date.now()), 4000);
-        setOffSetX(changeArray(offSetValues));
+        setTimeout(() => {}, 15000);
+    });
+
+    useEffect(() => {
+        prevOffSetX.current = offSetX;
+        count++;
+        let interval;
+
+        if (clicked === true) {
+            clicked = false;
+            setTimeout(() => {
+                interval = setInterval(() => setTime(Date.now()), 6000);
+            }, 9000);
+        } else {
+            interval = setInterval(() => setTime(Date.now()), 6000);
+        }
+
+        if (count > 1) {
+            setOffSetX(changeArray(offSetX));
+        }
         return () => {
             clearInterval(interval);
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [time]);
 
+    const handleLeftTransform = () => {
+        clicked = true;
+        setOffSetX(changeArray(offSetX));
+        for (let i = 0; i < galleryItems.current.length; i++) {
+            galleryItems.current[i].style.transform = `translateX(${offSetX[i]}%)`;
+        }
+    };
+
+    const handleRightTransform = () => {
+        setOffSetX(prevOffSetX.current);
+        for (let i = 0; i < galleryItems.current.length; i++) {
+            galleryItems.current[i].style.transform = `translateX(${offSetX[i]}%)`;
+        }
+    };
+
     const renderItem = props.map((item, i) => {
         return (
             <div
-                ref={(el) => (galleryItem.current[i] = el)}
+                ref={(el) => (galleryItems.current[i] = el)}
                 style={{ transform: `translateX(${offSetX[i]}%)` }}
                 key={item.id}
                 className={cx('gallery-item')}
@@ -61,13 +96,13 @@ function GallerySection({ props }) {
             <div className={cx('gallery-container')}>
                 <div className={cx('gallery-wrapper')}>
                     <div className={cx('gallery-prev')}>
-                        <button className={cx('btn', 'prev-btn')}>
+                        <button onClick={handleLeftTransform} className={cx('btn', 'prev-btn')}>
                             <i className={cx('icon', 'go-left-icon')}></i>
                         </button>
                     </div>
                     {renderItem}
                     <div className={cx('gallery-next')}>
-                        <button className={cx('btn', 'next-btn')}>
+                        <button onClick={handleRightTransform} className={cx('btn', 'next-btn')}>
                             <i className={cx('icon', 'go-right-icon')}></i>
                         </button>
                     </div>
